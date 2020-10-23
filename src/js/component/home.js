ef7dfdd718691a3228b02405c4a5dde51b4f218c
crduque <crduque@hotmail.com>
2020-10-23 09:46:39 +0000

    Added modal and restart button

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Button } from "./button.jsx";
+import { Modal } from "./modal.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,19 +8,9 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="">
+			<h1>TicTacToe in React.js</h1>
+			<Modal />
 		</div>
 	);
 }
---
+++ src/js/component/modal.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Button } from "./button.jsx";
+import { useEffect } from "react";
+
+export const Modal = props => {
+	const [opened, setOpened] = useState(true);
+	const [player, setPlayer] = useState(1);
+	return opened ? (
+		<div>
+			<h2>Choose your weapon</h2>
+			<button
+				type="button"
+				onClick={() => {
+					setOpened(false);
+					setPlayer(0);
+				}}>
+				X
+			</button>
+			<button
+				type="button"
+				onClick={() => {
+					setOpened(false);
+					setPlayer(0);
+				}}>
+				O
+			</button>
+		</div>
+	) : (
+		<button
+			type="button"
+			onClick={() => {
+				window.location.reload(false);
+			}}>
+			Restart
+		</button>
+	);
+};
